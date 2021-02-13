8ae7988c98e509350c45e818df4623ffd8b1eacb
SANTIAGO GINABREDA <sginabreda@santandertecnologia.com.ar>
2021-02-13 18:02:44 -0300

    modified db initial script and xml mappers

--- src/main/resources/db/migration/V1__baseline.sql
+++ src/main/resources/db/migration/V1__baseline.sql
@@ -1,5 +1,18 @@
 CREATE TABLE property (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  rent_price FLOAT NOT NULL
+  create_time VARCHAR,
+  property_type VARCHAR,
+  rent_price FLOAT NOT NULL,
+  email_address VARCHAR,
+  code VARCHAR
 -- TODO: Add missing columns, indices, constraints
+);
+
+CREATE TABLE address (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  street VARCHAR,
+  city VARCHAR,
+  us_state VARCHAR,
+  zip VARCHAR,
+  timezone VARCHAR
 );
